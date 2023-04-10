c7989dcecefaa221335e1580d85161fba1b513df
Nathan Amin <nathanamin@gmail.com>
2023-04-10 13:39:02 -0700

    include line breaks

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def chat_api():
 
 
     messages = [
-        {"role": "system", "content": "You are an author tasked with writing decodable stories for children at their specific phonics reading levels. The user will input a phonics level and a topic. Write a 10 sentence story given that information."+ "Write a children's story at a phonics level of "+level+" about "+ topic+" . Be sure to include line breaks and paragrpahs."},
+        {"role": "system", "content": "You are an author tasked with writing decodable stories for children at their specific phonics reading levels. The user will input a phonics level and a topic. Write a 10 sentence story given that information."+ "Write a children's story at a phonics level of "+level+" about "+ topic+" . Ensure there is proper punctuation and line breaks between paragraphs."},
     ]
 
     response = openai.ChatCompletion.create(
